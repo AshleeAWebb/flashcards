eb6d045e1d3a425d20ab67edd13039a9d928d187
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-03-21 17:35:18 -0400

    Feat:Add Deck files

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,6 +5,10 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe ('Turn', function () {
+    it('should be a function', function() {
+        const turn = new Turn();
+        expect(Turn).to.be.a('function')
+    })
     it('should be an instance of Turn', function() {
         const turn = new Turn();
         expect(turn).to.be.an.instanceof(Turn);
