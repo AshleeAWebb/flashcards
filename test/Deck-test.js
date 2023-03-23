45c3a8a332190515b73223d229dcefdf081032e9
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-03-23 19:26:16 -0400

    REFACT: Upadte all functions to ES6

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
-describe('Deck', function () {
+describe('Deck', () => {
     let deck;
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
@@ -14,19 +14,19 @@ describe('Deck', function () {
         deck = new Deck([card1, card2, card3]);
     });
 
-    it('should be a function', function () {
+    it('should be a function', () => {
         expect(Deck).to.be.a('function');
     });
 
-    it('should be an instance of Deck', function () {
+    it('should be an instance of Deck', () => {
         expect(deck).to.be.an.instanceOf(Deck);
     });
 
-    it('should be initialized with an array of Card objects', function () {
+    it('should be initialized with an array of Card objects', () => {
         expect(deck.cards).to.deep.equal([card1, card2, card3]);
     });
 
-    it('should be able to count how many cards are in the deck', function () {
+    it('should be able to count how many cards are in the deck', () => {
         expect(deck.countCards()).to.equal(3);
     });
 });
