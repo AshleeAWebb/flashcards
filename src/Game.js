3cb1dd91352915826b98e0a89e7834d8f62a7c0f
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-03-22 20:18:38 -0400

    Refact: Add beforeEach to all test to improve DRY and SRP of testing code.

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,23 @@
-const data = require('./data');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const data = require('./data')
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
+start() {
+  const cards = prototypeData.map(cardData => new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer));
 
+  const deck = new Deck(cards);
+  this.currentRound = new Round(deck);
+  this.printMessage(deck, this.currentRound);
+  this.printQuestion(this.currentRound);
+
+}
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
@@ -14,5 +27,6 @@ class Game {
       util.main(round);
   }
 }
+
 
 module.exports = Game;
